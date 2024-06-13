27f9b19027ce8dcee64ee18de66f9852f6f081f5
AndreasKendziorra <andreas.kendziorra@lightcurve.io>
2024-06-13 14:19:13 +0200

    Fix compuation regarding capped dailyRewards

--- APR.py
+++ APR.py
@@ -8,6 +8,8 @@ def getAverageLockingDuration(totalAmountStaked, totalWeight, offset):
 # This function calculates the current expected average APR 
 # Should be shown after multiplied by 100%
 def calculateCurrentAverageAPR(totalAmountStaked, totalWeight, dailyRewards):
+    dailyRewards = min(totalAmountStaked/365, dailyRewards)
+    print("Current daily rewards:", dailyRewards)
     averageLockingDuration = getAverageLockingDuration(totalAmountStaked, totalWeight, offset)
     expectedTotalRewardPerLSK = ( offset + (averageLockingDuration +1) / 2) * averageLockingDuration * dailyRewards/totalWeight
     durationAsFractionOfYear = averageLockingDuration/365       
@@ -15,15 +17,17 @@ def calculateCurrentAverageAPR(totalAmountStaked, totalWeight, dailyRewards):
 
 
 # Expected APR for a new position
-def calculateMyExpectedAPRForNewPosition(totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset):
-    expectedTotalReward = calculateExpectedTotalRewards(totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset)
+def calculateMyExpectedAPRForNewPosition(totalAmountStaked, totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset):
+    expectedTotalReward = calculateExpectedTotalRewards(totalAmountStaked, totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset)
     gainAsFraction = expectedTotalReward/newAmountStaked # needs to be multiplied by 100 to get the % value
     durationAsFractionOfYear = lockingDuration/365       # needs to be multiplied by 100 to get the % value
     return gainAsFraction / durationAsFractionOfYear
 
 
 # Estimated total rewards, non paused positions. 
-def calculateExpectedTotalRewards(totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset):
+def calculateExpectedTotalRewards(totalAmountStaked, totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset):
+    dailyRewards = min((totalAmountStaked+newAmountStaked)/365, dailyRewards)
+    print("Daily Rewards after new vote:", dailyRewards)
     totalWeight += newAmountStaked * (lockingDuration + offset) 
     expectedTotalReward = 0
     for i in range(lockingDuration):
@@ -36,18 +40,17 @@ def calculateExpectedTotalRewards(totalWeight, dailyRewards, newAmountStaked, lo
 
 
 # sample values
-totalAmountStaked = 500 # * 10 ** 6   # get from the contract
+totalAmountStaked = 282000000000000000000 / 10** 18 # * 10 ** 6   # get from the contract
 dailyRewards = 8000000 / 365    # get from the contract
 offset = 150                    # get from the contract
-totalWeight = 295000 #* 10 ** 6       # get from the contract
+totalWeight = 246335000000000000000000 / 10**18 #* 10 ** 6       # get from the contract
 
-dailyRewards = min(totalAmountStaked/365, dailyRewards)
 print("Current Avg APR: {}% (based on current staking participation and average locking duration of {} days)".format(calculateCurrentAverageAPR(totalAmountStaked, totalWeight, dailyRewards)*100, getAverageLockingDuration(totalAmountStaked, totalWeight, offset)))
 
 #sample values for new staking position
-newAmountStaked = 100 
-lockingDuration = 440
+newAmountStaked = 10 ** 6
+lockingDuration = 728
 print()
 print("New locking position with amount = {} and duration = {}:".format(newAmountStaked,lockingDuration))
-print("Expected APR:{}%".format(100*calculateMyExpectedAPRForNewPosition(totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset)) )
-print("Expected Total Rewards:", calculateExpectedTotalRewards(totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset))
+print("Expected APR:{}%".format(100*calculateMyExpectedAPRForNewPosition(totalAmountStaked, totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset)) )
+print("Expected Total Rewards:", calculateExpectedTotalRewards(totalAmountStaked, totalWeight, dailyRewards, newAmountStaked, lockingDuration, offset))
